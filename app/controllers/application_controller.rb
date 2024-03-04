361c18025867fd34af1604762a05e08e6abfe0ba
luke curtis <curtisluke67@gmail.com>
2024-03-04 15:58:40 +0400

    devise completed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,32 @@
 class ApplicationController < ActionController::Base
+before_action :authenticate_user!
+
+
+
+
+
+  # This is a method that is called before any other method in the controller
+
+
+
+
+
+
+
+
+
+
 end
+
+
+
+# before_action :configure_permitted_parameters, if: :devise_controller?
+
+# def configure_permitted_parameters
+#   # For additional fields in app/views/devise/registrations/new.html.erb
+#   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+
+#   # For additional in app/views/devise/registrations/edit.html.erb
+#   devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+# end
+# end
