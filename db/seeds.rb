58f3fd15a8c0ad6a9854ac689e4053662256db64
luke curtis <curtisluke67@gmail.com>
2024-03-05 15:01:43 +0400

    card

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
 
-# puts "Creating user"
-# user1 = User.new(name: "dummy", owner: "Yes", email: "dummy@mail.com",
-# password: "dummy123")
-# user1.save!
-# puts "dummy owner created successfully."
 
-puts "Creating new animals."
-animal1 = Animal.new(name: "Emberfury", abilities: "Fire Blast", age: 50, location: "St Pierre", price: 500, user_id: 2)
-animal1.save!
-puts "Seed data created successfully."
+#using faker
+10.times do
+
+  Animal.create!(
+    name: Faker::Creature::Animal.name,
+    abilities: Faker::Creature::Animal.name,
+    location: Faker::Address.city,
+    age: Faker::Number.between(from: 1, to: 10),
+    price: Faker::Number.between(from: 100, to: 1000),
+    user_id: 1
+  )
+end
