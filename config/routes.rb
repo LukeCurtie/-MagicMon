e9d9e71d398f87e893212bb24189179161ebf611
sanjivaniramburn@gmail.com <sanjivaniramburn@gmail.com>
2024-03-05 11:42:30 +0400

    AnimalController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # get 'animals/index'
+  # get 'animals/show'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -8,5 +10,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-#  root to: 'pages#index'
+  #  root to: 'pages#index'
+
+  resources :animals
 end
