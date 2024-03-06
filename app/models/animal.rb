85a7023456acb72de1167234fa739f7ba3e7ba08
luke curtis <curtisluke67@gmail.com>
2024-03-06 12:43:40 +0400

    validations

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,5 +1,10 @@
 class Animal < ApplicationRecord
   belongs_to :user
-  ABILITIES = %w(Speed Water Fire Air Earth Intellect Teleport)
+  ABILITIES = %w[Speed Water Fire Air Earth Intellect Teleport]
   validates :abilities, inclusion: { in: ABILITIES }
+  validates :name, :location, :age, :price, presence: true
+  validates :age, numericality: { only_integer: true, greater_than: 0 }
+  validates :price, numericality: { greater_than: 0 }
+  validates :name, uniqueness: true
+
 end
