858975f86b61611c93af111e99d88fe9b3a90221
luke curtis <curtisluke67@gmail.com>
2024-03-07 12:15:18 +0400

    show form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -7,26 +7,41 @@ class BookingsController < ApplicationController
     @animal = Animal.find(params[:animal_id])
     @booking = Booking.new
 
-
-
   end
 
+
+
+
+
+
   def create
+    @animal = Animal.find(params[:animal_id])
     @booking = Booking.new(booking_params)
     @booking.user = current_user
-    @booking.animal = Animal.find(params[:animal_id])
+    @booking.animal = @animal
+    @booking.total_price = total_price(@booking)
     if @booking.save
       redirect_to  @booking.animal
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
+
+
 
 
   private
 
   def booking_params
     params.require(:booking).permit(:start_date, :end_date)
+  end
+  def total_price(booking)
+    return unless booking.start_date && booking.end_date
+
+    number_of_days = (booking.end_date - booking.start_date).to_i
+    price_per_day = @animal.price
+
+    number_of_days * price_per_day
   end
 
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,27 @@ import "controllers";
 import "@popperjs/core";
 import "bootstrap";
 
+// Assuming your start_date and end_date fields have IDs 'start_date' and 'end_date'
+// and you have a span with the ID 'total_price' to display the result
+
+// document
+//   .getElementById("start_date")
+//   .addEventListener("change", calculateTotalPrice);
+// document
+//   .getElementById("end_date")
+//   .addEventListener("change", calculateTotalPrice);
+
+// const calculateTotalPrice = () => {
+//   const startDate = new Date(document.getElementById("start_date").value);
+//   const endDate = new Date(document.getElementById("end_date").value);
+//   const pricePerDay = animal.price;
+
+//   if (startDate && endDate) {
+//     const timeDifference = Math.abs(endDate.getTime() - startDate.getTime());
+//     const numberOfDays = Math.ceil(timeDifference / (1000 * 3600 * 24));
+//     const totalPrice = numberOfDays * pricePerDay;
+
+//     document.getElementById("total_price").innerText =
+//       "Total Price: " + totalPrice;
+//   }
+// };
